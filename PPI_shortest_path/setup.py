e94e4e004b688db83e0d6207be7e1b830ced12c6
SDD1695 <shiyongtai@sjtu.edu.cn>
2025-05-29 21:42:49 +0800

    Update setup.py

--- PPI_shortest_path/setup.py
+++ PPI_shortest_path/setup.py
@@ -1,1 +1,10 @@
 # 包安装配置
+from setuptools import setup, find_packages
+
+setup(
+    name="protein_network",
+    version="1.0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},  # 指定包根目录在 src 下
+    install_requires=[],      # 按需添加依赖项
+)
